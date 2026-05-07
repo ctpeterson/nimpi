6dba406cb941e94bab0a691a4a10927f0379d182
ctpeterson <curtistaylorpetersonwork@gmail.com>
2026-05-07 10:35:24 -0400

    First push of MPI communicator facilities

---
+++ src/nimpi/mpi.nim
@@ -0,0 +1,227 @@
+#[
+  NiMPI: https://github.com/ctpeterson/nimpi
+  Source: src/nimpi/mpi.nim
+  Author: Curtis Taylor Peterson <curtistaylorpetersonwork@gmail.com>
+
+  Acknowledgments:
+    - NimMPI by Michalina Kotwica (Udiknedormin)
+      https://github.com/Udiknedormin/NimMPI — MIT License
+      Copyright (c) 2016 M. Kotwica
+    - QEX (Quantum EXpressions) by James Osborn et al.
+      https://github.com/jcosborn/qex — MIT License
+      Copyright (c) 2015 James Osborn
+
+  MIT License
+
+  Copyright (c) 2026 Curtis Taylor Peterson
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+]#
+
+import mpiwrap
+
+export mpiwrap
+
+#[ MPI types ]#
+
+type MpiError* = object of CatchableError
+  ## Represents an error that occurs during MPI operations. Contains an error code
+  ## that can be used to identify the specific MPI error that occurred. Paired with
+  ## the `mpiAssert` procedure, which raises an `MpiError` when an MPI function returns
+  ## an error code.
+  ## 
+  ## Attributes:
+  ##  - `code`: The MPI error code associated with the error. 
+  code*: cint
+
+type 
+  MpiCommunicator* = object
+    ## Represents an MPI communicator, which is a group of processes that can 
+    ## communicate with each other. MPI communicators are fundamental to MPI 
+    ## programming, as they define the context for communication operations.
+    ## 
+    ## Attributes:
+    ## - `comm`: The underlying MPI_Comm handle that represents the communicator.
+    comm*: MPI_Comm
+
+#[ global variables ]#
+
+let
+  WorldCommunicator* = MpiCommunicator(comm: MPI_COMM_WORLD)
+    ## The default communicator that includes all processes in the MPI program.
+    
+  SelfCommunicator* = MpiCommunicator(comm: MPI_COMM_SELF)
+    ## A communicator that includes only the calling process. 
+
+#[ initialization/finalization ]#
+
+proc mpiInit* = 
+  ## Initializes the MPI environment. Must be called before any other MPI functions.
+  mpiAssert MPI_Init(nil, nil)
+
+proc mpiFinalize* = 
+  ## Finalizes the MPI environment. Should be called after all MPI functions.
+  mpiAssert MPI_Finalize()
+
+proc mpiInitialized*: bool = 
+  ## Checks if the MPI environment has been initialized.
+  var flag: cint
+  mpiAssert MPI_Initialized(addr flag)
+  return flag != 0
+
+proc mpiFinalized*: bool = 
+  ## Checks if the MPI environment has been finalized.
+  var flag: cint
+  mpiAssert MPI_Finalized(addr flag)
+  return flag != 0
+
+#[ error handling ]#
+
+proc mpiAssert*(code: cint) =
+  ## Asserts that an MPI function call was successful. If the code is not `MPI_SUCCESS`,
+  ## raises an `MpiError` with the corresponding error message.
+  ## 
+  ## Parameters:
+  ##  - `code`: The return code from an MPI function call to check for success.
+  ## 
+  ## Example:
+  ## ```nim
+  ## var rank: cint
+  ## mpiAssert MPI_Comm_rank(WorldCommunicator.comm, addr rank)
+  ## ```
+  if code != MPI_SUCCESS:
+    var buf: array[1024, char]
+    var len: cint
+    discard MPI_Error_string(code, cast[cstring](addr buf[0]), addr len)
+    var message = newString(len)
+    copyMem(addr message[0], addr buf[0], len)
+    var err = newException(MpiError, message)
+    err.code = code
+    raise err
+
+#[ timers ]#
+
+proc mpiWallTime*: float64 = 
+  ## Returns the current wall clock time in seconds. The time is measured from an 
+  ## arbitrary point in the past, and is only meaningful when compared to another 
+  ## value returned by `mpiWallTime`. This function is typically used for timing 
+  ## code sections in MPI programs.
+  return MPI_Wtime()
+
+proc mpiWallTick*: float64 = 
+  ## Returns the resolution of the timer used by `mpiWallTime`, in seconds. This value
+  ## represents the smallest measurable time interval that can be returned by `mpiWallTime`.
+  return MPI_Wtick()
+
+#[ communicator ]#
+
+proc duplicate*(communicator: MpiCommunicator): MpiCommunicator =
+  ## Duplicates the given communicator
+  ## 
+  ## Parameters:
+  ##  - `communicator`: The communicator to duplicate.
+  ## 
+  ## Returns:
+  ##  - A new `MpiCommunicator` representing the duplicated communicator.
+  ## 
+  ## Example:
+  ## ```nim
+  ## let comm = WorldCommunicator
+  ## let dupComm = comm.duplicate()
+  ## echo "Original communicator rank: ", comm.myRank, ", Duplicated communicator rank: ", dupComm.myRank
+  ## ```
+  var comm: MPI_Comm
+  mpiAssert MPI_comm_dup(communicator.comm, addr comm)
+  return MpiCommunicator(comm: comm)
+
+proc split*(communicator: MpiCommunicator; color, key: int): MpiCommunicator =
+  ## Creates a subcommunicator from `communicator` using `MPI_Comm_split`.
+  ##
+  ## Splits the given communicator into one or more subcommunicators based on 
+  ## the "color" and "key" values. "Color" determines the subcommunicator assignment; 
+  ## i.e., all processes passing the same value for "color" will be in the same 
+  ## subcommunicator. "Key" determines the rank ordering within the new communicator; 
+  ## i.e., processes with lower key values will have lower ranks in the new 
+  ## communicator - the processes with the lowest key is therefore assigned to rank 0.
+  ## 
+  ## Parameters:
+  ##  - `communicator`: The communicator to split.
+  ##  - `color`: Used to determine subcommunicator assignment. 
+  ##  - `key`: Used to determine rank ordering within the new communicator.
+  ## 
+  ## Returns:
+  ##  - A new `MpiCommunicator` subcommunicator
+  ## 
+  ## Example:
+  ## ```nim
+  ## let comm = WorldCommunicator
+  ## let subcomm = comm.split(comm.myRank mod 2, comm.myRank)
+  ## echo "Process ", comm.myRank, " is in subcommunicator with rank ", subcomm.myRank
+  ## ```
+  var comm: MPI_Comm
+  mpiAssert MPI_Comm_split(communicator.comm, cint(color), cint(key), addr comm)
+  return MpiCommunicator(comm: comm)
+
+proc abort*(communicator: MpiCommunicator, errorcode: int = 1) =
+  ## Aborts the MPI program with the given error code.
+  ## 
+  ## Parameters:
+  ##  - `communicator`: The communicator to use for the abort operation.
+  ##  - `errorcode`: The error code to return upon aborting (default is 1).
+  ## 
+  ## Example:
+  ## ```nim
+  ## let comm = WorldCommunicator
+  ## if comm.myRank == 0:
+  ##   comm.abort(42)  # Aborts the program with error code 42
+  ## ```
+  mpiAssert MPI_Abort(communicator.comm, cint(errorcode))
+
+proc free*(communicator: var MpiCommunicator) =
+  mpiAssert MPI_Comm_free(addr communicator.comm)
+
+proc size*(communicator: MpiCommunicator): int =
+  var size: cint
+  mpiAssert MPI_Comm_size(communicator.comm, addr size)
+  return int(size)
+
+proc myRank*(communicator: MpiCommunicator): int =
+  var rank: cint
+  mpiAssert MPI_Comm_rank(communicator.comm, addr rank)
+  return int(rank)
+
+#[ MPI block ]#
+
+template mpi*(body: untyped): untyped =
+  ## Encapsulates MPI program in a block that ensures proper initialization and 
+  ## finalization of MPI. Alternative to manual calls to `mpiInit` and `mpiFinalize`.
+  ## 
+  ## Example:
+  ## ```nim
+  ## mpi:
+  ##   let comm = WorldCommunicator
+  ##   echo "Hello from process ", comm.myRank, " of ", comm.size
+  ## ```
+  proc main =
+    mpiAssert MPI_Init(nil, nil)
+    defer: mpiAssert MPI_Finalize()
+    assert mpiInitialized()
+    body
+  main()
+  assert mpiFinalized()
---
+++ src/nimpi/mpiwrap.nim
@@ -0,0 +1,283 @@
+#[
+  NiMPI: https://github.com/ctpeterson/nimpi
+  Source: src/nimpi/mpiwrap.nim
+  Author: Curtis Taylor Peterson <curtistaylorpetersonwork@gmail.com>
+
+  Acknowledgments:
+    - NimMPI by Michalina Kotwica (Udiknedormin)
+      https://github.com/Udiknedormin/NimMPI — MIT License
+      Copyright (c) 2016 M. Kotwica
+    - QEX (Quantum EXpressions) by James Osborn et al.
+      https://github.com/jcosborn/qex — MIT License
+      Copyright (c) 2015 James Osborn
+
+  MIT License
+
+  Copyright (c) 2026 Curtis Taylor Peterson
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+]#
+
+import std/[macros]
+
+{.pragma: mpi, importc, header: "mpi.h".}
+
+#[ macros for slick definitions of types and constants ]#
+
+macro mpiTypes(definition, idents: untyped): untyped =
+  result = newStmtList()
+  for ident in idents: result.add newCall(definition, ident)
+
+macro mpiVariables(definition, idents: untyped): untyped =
+  result = newStmtList()
+  for ident in idents:
+    if ident[1].kind == nnkStmtList: 
+      result.add newCall(definition, ident[0], ident[1][0])
+    else: result.add newCall(definition, ident[0], ident[1])
+
+template typeDefinition(ident: untyped) {.dirty.} =
+  type ident* {.mpi.} = object
+
+template constantDefinition(ident: untyped, identType: untyped) {.dirty.} =
+  var ident* {.mpi.}: identType
+
+#[ type & constant definitions ]#
+
+type MPI_Offset* {.mpi.} = cint
+
+mpiTypes typeDefinition:
+  MPI_Aint
+  MPI_Count
+  MPI_Comm
+  MPI_Datatype
+  MPI_Errhandler
+  MPI_File
+  MPI_Group
+  MPI_Info
+  MPI_Op
+  MPI_Request
+  MPI_Message
+  MPI_Status
+  MPI_Win
+  MPI_Fint
+
+mpiVariables constantDefinition:
+  # communicator constants
+  MPI_COMM_WORLD: MPI_Comm
+  MPI_COMM_SELF: MPI_Comm
+  MPI_COMM_NULL: MPI_Comm
+
+  # group constants
+  MPI_GROUP_NULL: MPI_Group
+  MPI_GROUP_EMPTY: MPI_Group
+
+  # special rank values
+  MPI_ANY_SOURCE: cint
+  MPI_PROC_NULL: cint
+  MPI_ROOT: cint
+  MPI_UNDEFINED: cint
+
+  # reduction operations
+  MPI_MAX: MPI_Op
+  MPI_MIN: MPI_Op
+  MPI_SUM: MPI_Op
+  MPI_PROD: MPI_Op
+  MPI_LAND: MPI_Op
+  MPI_BAND: MPI_Op
+  MPI_LOR: MPI_Op
+  MPI_BOR: MPI_Op
+  MPI_LXOR: MPI_Op
+  MPI_BXOR: MPI_Op
+  MPI_MAXLOC: MPI_Op
+  MPI_MINLOC: MPI_Op
+  MPI_REPLACE: MPI_Op
+  MPI_NO_OP: MPI_Op
+  MPI_OP_NULL: MPI_Op
+
+  # datatypes
+  MPI_DATATYPE_NULL: MPI_Datatype
+  MPI_BYTE: MPI_Datatype
+  MPI_PACKED: MPI_Datatype
+  MPI_CHAR: MPI_Datatype
+  MPI_SHORT: MPI_Datatype
+  MPI_INT: MPI_Datatype
+  MPI_LONG: MPI_Datatype
+  MPI_FLOAT: MPI_Datatype
+  MPI_DOUBLE: MPI_Datatype
+  MPI_LONG_DOUBLE: MPI_Datatype
+  MPI_UNSIGNED_CHAR: MPI_Datatype
+  MPI_SIGNED_CHAR: MPI_Datatype
+  MPI_UNSIGNED_SHORT: MPI_Datatype
+  MPI_UNSIGNED: MPI_Datatype
+  MPI_UNSIGNED_LONG: MPI_Datatype
+  MPI_UNSIGNED_LONG_LONG: MPI_Datatype
+  MPI_LONG_LONG_INT: MPI_Datatype
+  MPI_INT8_T: MPI_Datatype
+  MPI_UINT8_T: MPI_Datatype
+  MPI_INT16_T: MPI_Datatype
+  MPI_UINT16_T: MPI_Datatype
+  MPI_INT32_T: MPI_Datatype
+  MPI_UINT32_T: MPI_Datatype
+  MPI_INT64_T: MPI_Datatype
+  MPI_UINT64_T: MPI_Datatype
+  MPI_C_BOOL: MPI_Datatype
+  MPI_C_FLOAT_COMPLEX: MPI_Datatype
+  MPI_C_DOUBLE_COMPLEX: MPI_Datatype
+  MPI_FLOAT_INT: MPI_Datatype
+  MPI_DOUBLE_INT: MPI_Datatype
+  MPI_LONG_INT: MPI_Datatype
+  MPI_SHORT_INT: MPI_Datatype
+  MPI_2INT: MPI_Datatype
+
+  # Request / status
+  MPI_REQUEST_NULL: MPI_Request
+  MPI_MESSAGE_NULL: MPI_Message
+  MPI_MESSAGE_NO_PROC: MPI_Message
+  MPI_STATUS_IGNORE: ptr MPI_Status
+  MPI_STATUSES_IGNORE: ptr MPI_Status
+
+  # Info
+  MPI_INFO_NULL: MPI_Info
+
+  # Errhandler
+  MPI_ERRHANDLER_NULL: MPI_Errhandler
+  MPI_ERRORS_ARE_FATAL: MPI_Errhandler
+  MPI_ERRORS_RETURN: MPI_Errhandler
+
+  # Misc
+  MPI_IN_PLACE: pointer
+  MPI_BOTTOM: pointer
+  MPI_BSEND_OVERHEAD: cint
+  MPI_UNDEFINED: cint
+  MPI_MAX_PROCESSOR_NAME: cint
+  MPI_MAX_ERROR_STRING: cint
+  MPI_MAX_OBJECT_NAME: cint
+  MPI_MAX_LIBRARY_VERSION_STRING: cint
+
+  # Error codes
+  MPI_SUCCESS: cint
+  MPI_ERR_BUFFER: cint
+  MPI_ERR_COUNT: cint
+  MPI_ERR_TYPE: cint
+  MPI_ERR_TAG: cint
+  MPI_ERR_COMM: cint
+  MPI_ERR_RANK: cint
+  MPI_ERR_REQUEST: cint
+  MPI_ERR_ROOT: cint
+  MPI_ERR_GROUP: cint
+  MPI_ERR_OP: cint
+  MPI_ERR_TOPOLOGY: cint
+  MPI_ERR_DIMS: cint
+  MPI_ERR_ARG: cint
+  MPI_ERR_UNKNOWN: cint
+  MPI_ERR_TRUNCATE: cint
+  MPI_ERR_OTHER: cint
+  MPI_ERR_INTERN: cint
+  MPI_ERR_IN_STATUS: cint
+  MPI_ERR_PENDING: cint
+  MPI_ERR_LASTCODE: cint
+
+  # Thread support levels
+  MPI_THREAD_SINGLE: cint
+  MPI_THREAD_FUNNELED: cint
+  MPI_THREAD_SERIALIZED: cint
+  MPI_THREAD_MULTIPLE: cint
+
+  # Topology types
+  MPI_CART: cint
+  MPI_GRAPH: cint
+  MPI_DIST_GRAPH: cint
+
+  # Communicator comparison results
+  MPI_IDENT: cint
+  MPI_CONGRUENT: cint
+  MPI_SIMILAR: cint
+  MPI_UNEQUAL: cint
+
+  # File constants
+  MPI_FILE_NULL: MPI_File
+  MPI_MODE_RDONLY: cint
+  MPI_MODE_RDWR: cint
+  MPI_MODE_WRONLY: cint
+  MPI_MODE_CREATE: cint
+  MPI_MODE_APPEND: cint
+
+#[ initialization/finalization ]#
+
+proc MPI_Init*(argc: ptr cint, argv: ptr cstringArray): cint {.cdecl, mpi.}
+
+proc MPI_Initialized*(flag: ptr cint): cint {.cdecl, mpi.}
+
+proc MPI_Finalize*: cint {.cdecl, mpi.}
+
+proc MPI_Finalized*(flag: ptr cint): cint {.cdecl, mpi.}
+
+#[ error handling ]#
+
+proc MPI_Error_string*(
+  code: cint, 
+  str: cstring, 
+  resultlen: ptr cint
+): cint {.cdecl, mpi.}
+
+#[ timers ]#
+
+proc MPI_Wtime*: cdouble {.cdecl, mpi.}
+
+proc MPI_Wtick*: cdouble {.cdecl, mpi.}
+
+#[ MPI communicator ]#
+
+proc MPI_Abort*(comm: MPI_Comm, errorcode: cint): cint {.cdecl, mpi.}
+
+proc MPI_Comm_rank*(comm: MPI_Comm, rank: ptr cint): cint {.cdecl, mpi.}
+
+proc MPI_Comm_size*(comm: MPI_Comm, size: ptr cint): cint {.cdecl, mpi.}
+
+proc MPI_comm_dup*(comm: MPI_Comm, newcomm: ptr MPI_Comm): cint {.cdecl, mpi.}
+
+proc MPI_Comm_split*(
+  comm: MPI_Comm, 
+  color: cint, 
+  key: cint, 
+  newcomm: ptr MPI_Comm
+): cint {.cdecl, mpi.}
+
+proc MPI_Comm_free*(comm: ptr MPI_Comm): cint {.cdecl, mpi.}
+
+proc MPI_Comm_create*(
+  comm: MPI_Comm, 
+  group: MPI_Group, 
+  newcomm: ptr MPI_Comm
+): cint {.cdecl, mpi.}
+
+proc MPI_Comm_compare*(
+  comm1: MPI_Comm, 
+  comm2: MPI_Comm, 
+  result: ptr cint
+): cint {.cdecl, mpi.}
+
+proc MPI_Comm_group*(comm: MPI_Comm, group: ptr MPI_Group): cint {.cdecl, mpi.}
+
+
+
+
+
+
+
